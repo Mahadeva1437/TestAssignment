22939f01283135a225764f9ee7c273fcd2f8808c
Mahandeva1437 <mahanteshsb460@gmail.com>
2025-03-19 00:32:47 +0530

    first commit

---
+++ src/test/java/com/qa/testcases/patinfoTest.java
@@ -0,0 +1,27 @@
+package com.qa.testcases;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+
+import com.qa.pages.patInfoPage;
+import com.qa.base.TestBase;
+
+public class patinfoTest extends TestBase{
+
+	
+
+	@Test
+	public void Homepage() throws InterruptedException{		
+		patInfoPage page =new patInfoPage(getDriver());
+		
+		page.endtoendcases();
+//		Home.logout();
+	
+
+}
+	
+	
+}
+
+
